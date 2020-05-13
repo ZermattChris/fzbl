4573cac2e6a5682a40128043e917ea53ef771c3c
ZermattChris <chris@flyzermatt.com>
2020-05-13 20:15:09 +0200

    Trying to get a centered layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app id="inspire"><v-app id="inspire">
+
+      <v-content>
+        <v-container
+            class="fill-height"
+            fluid
+          >
+            <v-row align="center" justify="center" style="border:1px maroon solid;" >
+              <v-col cols="12" sm="8" md="4" >
+
+                <v-card class="elevation-12">
+                  <v-toolbar color="primary" dark flat >
+                    <v-toolbar-title>
+                      <FZFormHeader/>
+                    </v-toolbar-title>
+                  </v-toolbar>
+
+                  <FZFormBody/>
+                  <FZFormFooter/>
+
+                </v-card>
+
+              </v-col>
+            </v-row>
+
+        </v-container>
+      </v-content>
+
+    </v-app></v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FZFormHeader from './components/FZFormHeader';
+import FZFormBody from './components/FZFormBody';
+import FZFormFooter from './components/FZFormFooter';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    FZFormHeader,
+    FZFormBody,
+    FZFormFooter
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/FZFormBody.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>body</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
